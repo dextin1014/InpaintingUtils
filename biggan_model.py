743bea6006a0498f3861d1258cc9094144516848
dextin1014 <53894536+dextin1014@users.noreply.github.com>
2021-09-22 23:26:27 +0900

    Update biggan_model.py

--- biggan_model.py
+++ biggan_model.py
@@ -34,7 +34,7 @@ def image_to_image_generator():
     inputs = layers.Input((256, 256, 3))
     x = inputs
     encoders = []
-    for ch in [64, 128, 256]:
+    for ch in [256, 512, 1024]:
         x = ConvSN2D(ch, 3, padding="same", strides=2)(x)
         x = InstanceNorm2D()(x)
         x = layers.ReLU()(x)
@@ -42,13 +42,13 @@ def image_to_image_generator():
     for d in [1, 1, 2, 2]:
         x = ConvSN2D(512, 3, padding="same", dilation_rate=d)(x)
         x = InstanceNorm2D()(x)
-        x = layers.ReLU()(x) # (32, 32, 512)
+        x = layers.ReLU()(x) # (32, 32, 2048)
     x = layers.Concatenate()([x, encoders[-1]])
-    x = biggan_residual_block(x, 256, -2, True)  # (64, 64, 256)
+    x = biggan_residual_block(x, 1024, -2, True)  # (64, 64, 1024)
     x = layers.Concatenate()([x, encoders[-2]])
-    x = biggan_residual_block(x, 128, -2, True)  # (128, 128, 128)
+    x = biggan_residual_block(x, 512, -2, True)  # (128, 128, 512)
     x = layers.Concatenate()([x, encoders[-3]])
-    x = biggan_residual_block(x, 64, -2, True)  # (256, 256, 64)
+    x = biggan_residual_block(x, 256, -2, True)  # (256, 256, 256)
     x = InstanceNorm2D()(x)
     x = layers.ReLU()(x)
     x = ConvSN2D(3, 3, padding="same", activation="tanh")(x)
@@ -57,10 +57,10 @@ def image_to_image_generator():
 
 def discriminator():
     inputs = layers.Input((256, 256, 3))
-    x = biggan_residual_block(inputs, 128, 2, False) # (128, 128, 128)
-    x = biggan_residual_block(x, 256, 2, False)  # (64, 64, 256)
-    x = biggan_residual_block(x, 512, 2, False)  # (32, 32, 512)
-    x = biggan_residual_block(x, 512, 1, False)
+    x = biggan_residual_block(inputs, 256, 2, False) # (128, 128, 256)
+    x = biggan_residual_block(x, 512, 2, False)  # (64, 64, 512)
+    x = biggan_residual_block(x, 1024, 2, False)  # (32, 32, 1024)
+    x = biggan_residual_block(x, 1024, 1, False)
     x = layers.ReLU()(x)
     x = ConvSN2D(1, 1)(x)
     model = tf.keras.models.Model(inputs, x)
